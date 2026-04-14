c94d2d49c1542c63552f6b59bfc1148136e7c784
vasirajaread <vasirajaread@gmail.com>
2026-04-14 23:42:12 +0530

    add purpose

--- taskpro/src/app/components/add/add.component.ts
+++ taskpro/src/app/components/add/add.component.ts
@@ -10,6 +10,7 @@ import { MatCard, MatCardContent, MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
+import { SnackbarService } from '../../services/snackbar.service';
 
 @Component({
   selector: 'app-add',
@@ -39,18 +40,31 @@ export class AddComponent {
     dob: '',
     gender: '',
   };
-
+  classAgeMap: any = {
+    '1': { min: 5, max: 7 },
+    '2': { min: 6, max: 8 },
+    '3': { min: 7, max: 9 },
+    '4': { min: 8, max: 10 },
+    '5': { min: 9, max: 11 },
+    '6': { min: 10, max: 12 },
+    '7': { min: 11, max: 13 },
+    '8': { min: 12, max: 14 },
+    '9': { min: 13, max: 15 },
+    '10': { min: 14, max: 16 },
+    '11': { min: 15, max: 17 },
+    '12': { min: 16, max: 18 }
+  };
   constructor(
     private productservice: ProductsService,
-    private studentservice: StudentsService
+    private studentservice: StudentsService,
+    public snackbar: SnackbarService
   ) { }
 
-  onSubmitProduct(form: NgForm) {
-    if (form.invalid) return;
+  addProduct(form: NgForm) {
     console.log(this.products)
     this.productservice.addProduct(this.products).subscribe({
       next: (res: any) => {
-        console.log('Product added', res);
+        this.snackbar.openSnackBar("Product Added")
         this.resetForm(form);
       },
       error: (err: any) => {
@@ -59,11 +73,25 @@ export class AddComponent {
     });
   }
 
-  onSubmitStudent(form: NgForm) {
-    if (form.invalid) return;
+  addStudent(form: NgForm) {
+
+    const age = this.calculateAge(this.student.dob);
+    const selectedClass = this.student.class;
+
+    const selectedone = this.classAgeMap[selectedClass];
+
+
+
+    if (age < selectedone.min || age > selectedone.max) {
+      alert('Age is not valid or this specific class');
+      return;
+    }
+
     this.studentservice.addStudent(this.student).subscribe({
       next: (res: any) => {
-        console.log('Student added', res);
+        console.log(res.studentname)
+        this.snackbar.openSnackBar("Student Added");
+
         this.resetForm(form);
       },
       error: (err: any) => {
@@ -71,7 +99,32 @@ export class AddComponent {
       }
     });
   }
+  calculateAge(dob: string): number {
 
+    const newDate = new Date();
+    const birthDate = new Date(dob);
+
+    const birthyear = birthDate.getFullYear();
+    const currentYear = newDate.getFullYear();
+
+    let ageCalc = currentYear - birthyear;
+
+    const birthMonth = birthDate.getMonth() + 1;
+    const currentMonth = newDate.getMonth() + 1;
+
+    if (birthMonth > currentMonth) {
+      ageCalc = ageCalc - 1;
+    }
+    else if (birthMonth === currentMonth) {
+
+      if (birthDate.getDate() > newDate.getDate()) {
+        ageCalc = ageCalc - 1;
+      }
+
+    }
+
+    return ageCalc;
+  }
   resetForm(form: NgForm) {
     form.reset();
     this.products = { title: '', price: '', category: '', stock: '', overallrating: '' };
